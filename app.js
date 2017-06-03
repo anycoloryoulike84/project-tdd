59e331a0678471914b80a2017b3defdae6aeb95f
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-03 12:01:59 -0700

    npm shrinkwrap

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+var public = __dirname + "/public/";
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public + "index.html"));
+});
+
+app.use('/', express.static(public));
+
+app.listen(8080);
+
