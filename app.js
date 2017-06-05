3081d450484434e43f401f2f727672bcdf3609b6
anycoloryoulike84 <anycoloryoulike84@users.noreply.github.com>
2017-06-04 17:21:51 -0700

    added the loading_time.js file to log request times

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+var loadingTime = require("./loading_time");
 
+app.use(loadingTime);
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(express.static(__dirname + '/public'));
 
 var cities = require('./routes/cities');
